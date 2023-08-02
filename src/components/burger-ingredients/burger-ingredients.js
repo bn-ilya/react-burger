be4aaf10303234f05b0f1c0c66ba70a8c7264404
bn-ilya <bn.ilyaa@yandex.ru>
2023-08-02 16:39:16 +0300

    Сверстаны табы в ингредиентах

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,9 +1,29 @@
+import {useState} from 'react';
 import styles from './burger-ingredients.module.css';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components/dist/ui/tab'
+
+function Tabs() {
+    const [current, setCurrent] = useState('Булки');
+    return (
+        <div className={styles.tabs}>
+            <Tab value="Булки" active={current === 'Булки'} onClick={setCurrent}>
+                Булки
+            </Tab>
+            <Tab value="Соусы" active={current === 'Соусы'} onClick={setCurrent}>
+                Соусы
+            </Tab>
+            <Tab value="Начинки" active={current === 'Начинки'} onClick={setCurrent}>
+                Начинки
+            </Tab>
+        </div>
+    )
+}
 
 export default function BurgerIngredients(props) {
-    return(
+    return (
         <section className={styles.content + ' pt-10 ' + props.classes}>
             <h1 className='text text_type_main-large mb-5'>Соберите бургер</h1>
+            <Tabs />
         </section>
     )
 }
