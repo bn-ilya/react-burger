dc259c61b0947c25eda9c55e76c7c4307f43db9b
bn-ilya <bn.ilyaa@yandex.ru>
2023-08-04 16:22:19 +0300

    Доработан burrger ingredients

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,9 +1,10 @@
 import styles from './burger-constructor.module.css';
+import {ConstructorElement} from '@ya.praktikum/react-developer-burger-ui-components/dist/ui/constructor-element';
 
 export default function BurgerConstructor() {
     return(
         <section className={styles.content}>
-            <button>sad</button>
+            <ConstructorElement/>
         </section>
     )
 }
--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -23,9 +23,13 @@ function Tabs() {
 export default function BurgerIngredients(props) {
     return (
         <section className={styles.content + ' pt-10 ' + props.classes}>
-            <h1 className='text text_type_main-large mb-5'>Соберите бургер</h1>
-            <Tabs />
-            <Ingredients/>
+            <div className={styles.header}>
+                <h1 className='text text_type_main-large mb-5'>Соберите бургер</h1>
+                <Tabs />
+            </div>
+            <div className={styles.main}>
+                <Ingredients />
+            </div>
         </section>
     )
 }
