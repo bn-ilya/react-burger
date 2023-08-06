dc5535fa7a13316e60f2ee7b3cf29fa2b9f2435d
bn-ilya <bn.ilyaa@yandex.ru>
2023-08-07 00:02:54 +0300

    Добавлен fetch Запрос

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,22 @@
-import React from 'react';
 import AppHeader from '../app-header/app-header';
 import AppMain from '../app-main/app-main';
 import styles from './app.module.css';
-import ingredientsData from '../../utils/data';
+import { useEffect, useState } from 'react';
 import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css';
 
-
 function App() {
+  const [ingredientsData, setIngredientsData] = useState(null);
+
+  useEffect(()=>{
+    fetch('https://norma.nomoreparties.space/api/ingredients')
+    .then(res => res.json())
+    .then(data => setIngredientsData(data.data))
+  }, [])
 
   return (
     <div className={styles.app}>
       <AppHeader/>
-      <AppMain ingredientsData={ingredientsData}/>
+      {ingredientsData && <AppMain ingredientsData={ingredientsData}/>}
     </div>
   );
 }
