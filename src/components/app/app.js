b1f410454551f76e2a8f0c7c7e34435c81dd74b9
bn-ilya <bn.ilyaa@yandex.ru>
2023-08-07 01:05:38 +0300

    Добавлена обработка неудачных запросов

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,19 +4,35 @@ import styles from './app.module.css';
 import { useEffect, useState } from 'react';
 import '@ya.praktikum/react-developer-burger-ui-components/dist/ui/box.css';
 
+const URL_GET_INGREDIENTS = 'https://norma.nomoreparties.space/api/ingredients';
+
 function App() {
   const [ingredientsData, setIngredientsData] = useState(null);
+  const [isLoadIng, setIsLoading] = useState(true);
+  const [isErrorLoading, setIsErrorLoading] = useState(false);
 
-  useEffect(()=>{
-    fetch('https://norma.nomoreparties.space/api/ingredients')
-    .then(res => res.json())
-    .then(data => setIngredientsData(data.data))
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch(URL_GET_INGREDIENTS)
+      .then(res => res.json())
+      .then(data => {
+        setIngredientsData(data.data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsErrorLoading(true);
+      })
   }, [])
 
   return (
     <div className={styles.app}>
-      <AppHeader/>
-      {ingredientsData && <AppMain ingredientsData={ingredientsData}/>}
+      {!isLoadIng && (
+        <>
+          <AppHeader />
+          {!isErrorLoading ? (<AppMain ingredientsData={ingredientsData} />) : (<p>'Произошла ошибка запроса...'</p>)}
+        </>
+      )}
     </div>
   );
 }
