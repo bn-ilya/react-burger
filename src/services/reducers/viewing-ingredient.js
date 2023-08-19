2a8fec8f49c9a45790160ab931bedd58e847db99
bn-ilya <bn.ilyaa@yandex.ru>
2023-08-19 20:07:20 +0300

    Добавление просматриваемоего ингредиента в хранилище

--- src/services/reducers/viewing-ingredient.js
+++ src/services/reducers/viewing-ingredient.js
@@ -1,16 +1,21 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+const initialState = {
+    viewingIngredient: {}
+}
+
 const viewingIngredientSlice = createSlice({
     name: 'viewingIngredient',
-    initialState: {
-        viewingIngredient: {}
-    },
+    initialState,
     reducers: {
         setViewingIngredient: (state, action) => {
             state.viewingIngredient = action.payload
+        },
+        clearViewingIngredient: (state) => {
+            state.viewingIngredient = initialState.viewingIngredient
         }
     }
 })
 
 export default viewingIngredientSlice.reducer;
-export const {setViewingIngredient} = viewingIngredientSlice.actions; 
+export const {setViewingIngredient, clearViewingIngredient} = viewingIngredientSlice.actions; 
