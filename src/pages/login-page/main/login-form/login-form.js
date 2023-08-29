791f735e7889e229777ba10b49d99d660f04f8eb
bn-ilya <bn.ilyaa@yandex.ru>
2023-08-29 19:01:11 +0300

    Реализован инпут для email и пароля с обработчиком события

--- src/pages/login-page/main/login-form/login-form.js
+++ src/pages/login-page/main/login-form/login-form.js
@@ -1,9 +1,41 @@
+import { useState } from "react"
+import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
+
 export default function LoginForm() {
-    return(
-        <form>
-            <input/>
-            <input/>
-            <input/>
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleInputs = e => {
+        const [value, name] = [e.target.value, e.target.name];
+        setFormData({ ...formData, [name]: value })
+    }
+
+    return (
+        <form style={{ color: 'white' }}>
+            <Input
+                type={'text'}
+                placeholder={'placeholder'}
+                onChange={handleInputs}
+                icon={'CurrencyIcon'}
+                name={'email'}
+                error={false}
+                errorText={'Ошибка'}
+                size={'default'}
+            />
+            <Input
+                type={'text'}
+                placeholder={'placeholder'}
+                onChange={handleInputs}
+                icon={'CurrencyIcon'}
+                name={'password'}
+                error={false}
+                errorText={'Ошибка'}
+                size={'default'}
+            />
+            {formData.email}
+            {formData.password}
         </form>
     )
 }
