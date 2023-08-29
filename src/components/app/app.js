a162642f293f737298da3c8d041de63495dc5db0
bn-ilya <bn.ilyaa@yandex.ru>
2023-08-29 20:13:16 +0300

    Добавлена страницы с регистрацией и восстановлением пароля

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {LoginPage, ConstructorPage} from '../../pages'
+import {LoginPage, ConstructorPage, RegisterPage, ResetPasswordPage} from '../../pages'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<ConstructorPage />} />
-        <Route path='/login' element={<LoginPage />}></Route>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/register' element={<RegisterPage />}/>
+        <Route path='/reset-password' element={<ResetPasswordPage />}/>
       </Routes>
     </Router>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import LoginPage from "./login-page/login-page";
 import ConstructorPage from "./constructor-page/constructor-page";
+import ResetPasswordPage from "./reset-password-page/reset-password-page";
+import RegisterPage from "./register-page/register-page";
 
-export {LoginPage, ConstructorPage}
+export {LoginPage, ConstructorPage, ResetPasswordPage, RegisterPage}
