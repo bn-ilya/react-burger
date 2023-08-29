777e07f4d0039d8fa6af219ff479f75569b2285d
bn-ilya <bn.ilyaa@yandex.ru>
2023-08-29 18:37:12 +0300

    Добавлен компонент Login-form

---
+++ src/pages/login-page/main/login-form/login-form.js
@@ -0,0 +1,9 @@
+export default function LoginForm() {
+    return(
+        <form>
+            <input/>
+            <input/>
+            <input/>
+        </form>
+    )
+}
--- src/pages/login-page/main/main.js
+++ src/pages/login-page/main/main.js
@@ -1,8 +1,10 @@
 import styles from './main.module.css';
+import LoginForm from './login-form/login-form';
 
 export default function Main() {
     return (
         <div className={styles.content}>
+            <LoginForm/>
         </div>
     )
 }
