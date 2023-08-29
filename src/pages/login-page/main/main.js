ce0f24c60503251142263e4050ce38c04bece3de
bn-ilya <bn.ilyaa@yandex.ru>
2023-08-29 20:00:54 +0300

    Добавлены дополнительные ссылки на страницу входа

--- src/pages/login-page/main/main.js
+++ src/pages/login-page/main/main.js
@@ -1,10 +1,12 @@
 import styles from './main.module.css';
 import LoginForm from './login-form/login-form';
+import AdditionalActions from './additional-actions/additional-actions';
 
 export default function Main() {
     return (
         <div className={styles.content}>
-            <LoginForm/>
+            <LoginForm />
+            <AdditionalActions />
         </div>
     )
 }
