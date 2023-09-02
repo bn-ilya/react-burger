fcb37a8442ea7840a825d96fa9e53c186ef2dd34
bn-ilya <bn.ilyaa@yandex.ru>
2023-09-02 11:14:56 +0300

    Запрос перенесен выше в компонент страницы

--- src/pages/profile-page/main/main.js
+++ src/pages/profile-page/main/main.js
@@ -1,17 +1,8 @@
 import styles from './main.module.css';
 import ProfileForm from './profile-form/profile-form';
 import ProfileMenu from './profile-menu/profile-menu';
-import { useDispatch } from 'react-redux';
-import { getUserData } from '../../../services/reducers/profile';
-import { useEffect } from 'react';
 
 export default function Main() {
-    const dispatch = useDispatch();
-
-    useEffect(()=>{
-        dispatch(getUserData());
-    }, []);
-    
     return (
         <div className={styles.main}>
             <div className={styles.content}>
--- src/pages/profile-page/profile-page.js
+++ src/pages/profile-page/profile-page.js
@@ -1,8 +1,18 @@
-import Header from "../../components/header/header"
+import Header from "../../components/header/header";
 import ContainerPage from "../../components/container-page/container-page";
-import Main from "./main/main"
+import Main from "./main/main";
+import { useDispatch } from 'react-redux';
+import { getUserData } from "../../utils/burger-api";
+import { useEffect } from 'react';
 
 export default function ProfilePage() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getUserData());
+    }, []);
+
     return (
         <ContainerPage>
             <Header />
