b605b94011c11aab1e8350fc47f5228b12aa548f
bn-ilya <bn.ilyaa@yandex.ru>
2023-09-07 23:01:26 +0300

    Добавлена валидация для forgot-passwort

--- src/pages/forgot-password-page/main/forgot-password-form/forgot-password-form.js
+++ src/pages/forgot-password-page/main/forgot-password-form/forgot-password-form.js
@@ -1,49 +1,41 @@
-import { Input, Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import { EmailInput, Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from './forgot-password-form.module.css';
 import { useDispatch } from "react-redux";
 import { forgotPassword } from "../../../../services/reducers/forgot-password";
 import { useNavigate } from "react-router-dom";
 import { openModal } from "../../../../services/reducers/modal";
 import { useState } from "react";
+import useFormAndValidation from "../../../../hooks/use-form-and-validation";
 
 export default function ForgotPasswordForm() {
     const dispatch = useDispatch();
     const navigate = useNavigate()
+    const { values, errors, isValid, handleChange } = useFormAndValidation();
 
-    const [email, setEmail] = useState('')
-
-    const handleInput = e => {
-        setEmail(e.target.value)
-    }
-
-    const handleSubmit = e => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        dispatch(forgotPassword(email))
-            .unwrap()
-            .then((response) => {
-                if (response.success)
-                    navigate('/reset-password', { replace: true })
-                else
-                    throw new Error(response.message)
-            })
-            .catch(error => {
-                dispatch(openModal({ content: error.message, type: 'error' }))
-            })
+        e.stopPropagation();
+        try {
+            const res = await dispatch(forgotPassword(values.email)).unwrap();
+            navigate('/reset-password', { replace: true })
+        } catch (error) {
+            dispatch(openModal({ content: error.message, type: 'error' }))
+        }
     }
 
     return (
         <form className={styles.form} onSubmit={handleSubmit}>
             <h1 className={'text text_type_main-medium ' + styles.title}>Восстановление пароля</h1>
-            <Input
-                type={'text'}
+            <EmailInput
                 placeholder={'E-mail'}
-                value={email}
-                onChange={handleInput}
-                error={false}
-                errorText={'Ошибка'}
+                value={values.email ?? ""}
+                name="email"
+                onChange={handleChange}
+                error={!!errors.email}
+                errorText={errors.email}
                 size={'default'}
             />
-            <Button htmlType="submit" type="primary" size="medium">
+            <Button disabled={!isValid} htmlType="submit" type="primary" size="medium">
                 Восстановить
             </Button>
         </form>
