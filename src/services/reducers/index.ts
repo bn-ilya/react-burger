797dbaa9124a5b5959c8d0edc917609295ad56f1
bn-ilya <bn.ilyaa@yandex.ru>
2023-09-16 09:58:18 +0300

    Добавлена типизация хука useDispatch и useSelector

--- src/services/reducers/index.ts
+++ src/services/reducers/index.ts
@@ -28,3 +28,6 @@ export const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV === 'development',
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
