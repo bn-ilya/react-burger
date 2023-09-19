9baba68f22af868bef825153f94062400ab677a1
bn-ilya <bn.ilyaa@yandex.ru>
2023-09-19 18:07:53 +0300

    Типизирован компонент Container-page

---
+++ src/components/container-page/container-page.tsx
@@ -0,0 +1,9 @@
+import styles from './container-page.module.css';
+import type ContainerPageType from './container-page-type';
+import { FC } from 'react';
+
+const ContainerPage: FC<ContainerPageType> = ({ children }) => {
+  return <div className={styles.container}>{children}</div>;
+}
+
+export default ContainerPage;
