e2618a213e6e3b27c20d8f3e7216078d43592530
bn-ilya <bn.ilyaa@yandex.ru>
2023-09-21 19:12:49 +0300

    Добавлен тип IIngredient

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,3 +3,19 @@ export enum EIngredients {
     SAUCE = 'sauce',
     MAIN = 'main'
 } 
+
+export interface IIngredient {
+    _id: string
+    name: string,
+    type: string,
+    proteins: number,
+    fat: number,
+    carbohydrates: number,
+    calories: number,
+    price: number,
+    image: string,
+    image_mobile: string,
+    image_large: string,
+    __v: number,
+  }
+  
