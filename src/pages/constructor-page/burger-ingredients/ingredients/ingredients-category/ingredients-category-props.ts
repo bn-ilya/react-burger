7dfd26981dee00fa44b51f57d52e8106a7a51cd9
bn-ilya <bn.ilyaa@yandex.ru>
2023-09-21 19:13:24 +0300

    Изменен IIngredientsCategory интерфейс

--- src/pages/constructor-page/burger-ingredients/ingredients/ingredients-category/ingredients-category-props.ts
+++ src/pages/constructor-page/burger-ingredients/ingredients/ingredients-category/ingredients-category-props.ts
@@ -1,5 +1,7 @@
+import { IIngredient } from "../../../../../utils/types";
+
 export interface IIngredientsCategory {
     id: string,
     name: string,
-    ingredients: Array<any>
+    ingredients: Array<IIngredient>
 }
