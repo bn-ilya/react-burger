13a98be3b081daefd2d62477680b6163fa014ee9
bn-ilya <bn.ilyaa@yandex.ru>
2023-09-23 19:50:38 +0300

    Типизирован компонент InputEdit

---
+++ src/components/input-edit/input-edit.tsx
@@ -0,0 +1,33 @@
+import { Input } from '@ya.praktikum/react-developer-burger-ui-components';
+import { FC, useRef, useState } from 'react';
+
+const InputEdit: FC<typeof Input> = (props) => {
+  const [disabled, setDisabled] = useState(true);
+  const [value, setValue] = useState('');
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleIconClick = () => {
+    setDisabled(false);
+    if (inputRef.current instanceof HTMLInputElement) {
+      requestAnimationFrame(() => inputRef.current!.focus());
+    }
+  };
+
+  const handleBlurInput = () => {
+    setDisabled(true);
+  };
+
+  return (
+    <Input
+      ref={inputRef}
+      disabled={disabled}
+      onIconClick={handleIconClick}
+      onBlur={handleBlurInput}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      {...props}
+    />
+  );
+};
+
+export default InputEdit;
