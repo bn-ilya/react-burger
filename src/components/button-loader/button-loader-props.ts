57228632ca141ec9c7901138dda5ebfc737d6062
bn-ilya <bn.ilyaa@yandex.ru>
2023-09-23 18:53:17 +0300

    Типизирован компонент ButtonLoader

---
+++ src/components/button-loader/button-loader-props.ts
@@ -0,0 +1,7 @@
+import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ComponentProps } from 'react';
+
+export interface IButtonLoader extends ComponentProps<typeof Button> {
+  load: boolean;
+  disabled?: boolean;
+}
--- src/components/button-loader/button-loader.tsx
+++ src/components/button-loader/button-loader.tsx
@@ -1,22 +1,19 @@
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
-import { PropTypes } from 'prop-types';
+import { FC } from 'react';
 
+import { IButtonLoader } from './button-loader-props';
 import styles from './button-loader.module.css';
 
 import BurgerSpinLoader from '../ui/loaders/burger-spin-loader';
 
-export default function ButtonLoader({ load, children, disabled = null, ...props }) {
+const ButtonLoader: FC<IButtonLoader> = ({ load, children, disabled = false, ...props }) => {
   return (
     <Button disabled={load || disabled} extraClass={styles.loaderBtn} {...props}>
       {load && <BurgerSpinLoader type='secondary' />}
       {children}
     </Button>
   );
-}
-
-ButtonLoader.propTypes = {
-  load: PropTypes.bool,
-  children: PropTypes.oneOfType([PropTypes.node, PropTypes.element, PropTypes.string]),
-  disabled: PropTypes.bool,
 };
+
+export default ButtonLoader;
