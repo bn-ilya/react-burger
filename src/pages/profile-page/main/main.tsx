126b96be7c1d4f04a1affd2f6415dd156f1fa5a9
bn-ilya <bn.ilyaa@yandex.ru>
2023-10-07 13:46:41 +0300

    Изменены роуты у страницы profile

--- src/pages/profile-page/main/main.tsx
+++ src/pages/profile-page/main/main.tsx
@@ -1,13 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+
 import styles from './main.module.css';
 import ProfileForm from './profile-form/profile-form';
 import ProfileMenu from './profile-menu/profile-menu';
+
+import { ERoutes } from '../../../utils/types';
 
 const Main = () => {
   return (
     <div className={styles.main}>
       <div className={styles.content}>
         <ProfileMenu />
-        <ProfileForm />
+        <Routes>
+          <Route path={'/'} element={<ProfileForm />} />
+          <Route path={ERoutes.profileOrders} element={<ProfileForm />} />
+        </Routes>
       </div>
     </div>
   );
