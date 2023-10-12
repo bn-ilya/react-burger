fbf18229fba686f57364ba58315d415231842109
bn-ilya <bn.ilyaa@yandex.ru>
2023-10-13 00:29:42 +0300

    feat: add feed withdrawal from slice

--- src/pages/feed-page/main/feed-list/feed-list.tsx
+++ src/pages/feed-page/main/feed-list/feed-list.tsx
@@ -3,15 +3,17 @@ import { FC } from 'react';
 import styles from './feed-list.module.css';
 
 import FeedCart from '../../../../components/feed-cart/feed-cart';
+import { useAppSelector } from '../../../../hooks/rtk-hooks';
+import { selectFeeds } from '../../../../services/selectors';
 
 const FeedList: FC = () => {
+  const feeds = useAppSelector(selectFeeds);
+
   return (
     <div className={styles.content}>
-      <FeedCart />
-      <FeedCart />
-      <FeedCart />
-      <FeedCart />
-      <FeedCart />
+      {feeds.map((feed) => (
+        <FeedCart key={feed._id} />
+      ))}
     </div>
   );
 };
