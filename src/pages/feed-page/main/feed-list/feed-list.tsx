27bfb46f98bebd12c78f67a3d7a0c1d2bb071c79
bn-ilya <bn.ilyaa@yandex.ru>
2023-10-14 17:59:04 +0300

    feat: Добавлены проверка при получении данных feed

--- src/pages/feed-page/main/feed-list/feed-list.tsx
+++ src/pages/feed-page/main/feed-list/feed-list.tsx
@@ -1,13 +1,27 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import styles from './feed-list.module.css';
 
+import ErrorRequestPage from '../../../../components/error-request-page/error-request-page';
 import FeedCart from '../../../../components/feed-cart/feed-cart';
+import LoadingPage from '../../../../components/loading-page/loading-page';
 import { useAppSelector } from '../../../../hooks/rtk-hooks';
-import { selectFeeds } from '../../../../services/selectors';
+import {
+  selectFeeds,
+  selectIngredientsFailed,
+  selectIngredientsRequest,
+  selectWsFeedsConnected,
+} from '../../../../services/selectors';
 
 const FeedList: FC = () => {
   const feeds = useAppSelector(selectFeeds);
+
+  const isFetchIngredients = useAppSelector(selectIngredientsRequest);
+  const isFailedIngredients = useAppSelector(selectIngredientsFailed);
+  const isWsFeedsConnected = useAppSelector(selectWsFeedsConnected);
+
+  if (isFetchIngredients || !isWsFeedsConnected) return <LoadingPage />;
+  if (isFailedIngredients) return <ErrorRequestPage />;
 
   return (
     <div className={styles.content}>
