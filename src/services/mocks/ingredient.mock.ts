ad6a828bd17c742c03185431f7954928e8798d27
bn-ilya <bn.ilyaa@yandex.ru>
2023-10-29 13:26:54 +0300

    refactor: Исправление теста ingredients-constructor

---
+++ src/services/mocks/ingredient.mock.ts
@@ -0,0 +1,22 @@
+import { IIngredient, IIngredientConstructor } from '../../utils/types';
+
+export const mockIngredient: IIngredient = {
+  _id: '643d69a5c3f7b9001cfa093c',
+  name: 'Краторная булка N-200i',
+  type: 'bun',
+  proteins: 80,
+  fat: 24,
+  carbohydrates: 53,
+  calories: 420,
+  price: 1255,
+  image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+  image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+  image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
+  __v: 0,
+};
+
+export const mockIngredientConstructor: IIngredientConstructor = {
+  ...mockIngredient,
+  index: 0,
+  uniqueId: '643d69a5c3f7b9001cfa093c',
+};
--- src/services/reducers/ingredients-constructor/ingredients-constructor.test.ts
+++ src/services/reducers/ingredients-constructor/ingredients-constructor.test.ts
@@ -1,3 +1,4 @@
+import { mockIngredient, mockIngredientConstructor } from './../../mocks/ingredient.mock';
 import ingredientsConstructorReducer, {
   addIngredients,
   moveIngredients,
@@ -8,7 +9,7 @@ import ingredientsConstructorReducer, {
   type IInitialState,
 } from './ingredients-constructor';
 
-import { IIngredient, IIngredientConstructor } from '../../../utils/types';
+import { IIngredientConstructor } from '../../../utils/types';
 
 describe('ingredientsConstructor reducer', () => {
   const initialState: IInitialState = {
@@ -23,63 +24,18 @@ describe('ingredientsConstructor reducer', () => {
   });
 
   it('should handle setBunTop', () => {
-    const mockIngredient = {
-      _id: '643d69a5c3f7b9001cfa093c',
-      name: 'Краторная булка N-200i',
-      type: 'bun',
-      proteins: 80,
-      fat: 24,
-      carbohydrates: 53,
-      calories: 420,
-      price: 1255,
-      image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-      image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-      image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-      __v: 0,
-    };
-
     const action = setBunTop(mockIngredient);
     const expectedState = { ...initialState, bunTop: mockIngredient };
     expect(ingredientsConstructorReducer(initialState, action)).toEqual(expectedState);
   });
 
   it('should handle setBunBottom', () => {
-    const mockIngredient: IIngredient = {
-      _id: '643d69a5c3f7b9001cfa093c',
-      name: 'Краторная булка N-200i',
-      type: 'bun',
-      proteins: 80,
-      fat: 24,
-      carbohydrates: 53,
-      calories: 420,
-      price: 1255,
-      image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-      image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-      image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-      __v: 0,
-    };
-
     const action = setBunBottom(mockIngredient);
     const expectedState = { ...initialState, bunBottom: mockIngredient };
     expect(ingredientsConstructorReducer(initialState, action)).toEqual(expectedState);
   });
 
   it('should handle addIngredients', () => {
-    const mockIngredient: IIngredient = {
-      _id: '643d69a5c3f7b9001cfa093c',
-      name: 'Краторная булка N-200i',
-      type: 'bun',
-      proteins: 80,
-      fat: 24,
-      carbohydrates: 53,
-      calories: 420,
-      price: 1255,
-      image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-      image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-      image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-      __v: 0,
-    };
-
     const action = addIngredients(mockIngredient);
 
     const expectedState = {
@@ -99,18 +55,7 @@ describe('ingredientsConstructor reducer', () => {
     const state: IInitialState = {
       ingredients: [
         {
-          _id: '643d69a5c3f7b9001cfa093c',
-          name: 'Краторная булка N-200i',
-          type: 'bun',
-          proteins: 80,
-          fat: 24,
-          carbohydrates: 53,
-          calories: 420,
-          price: 1255,
-          image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-          image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-          image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-          __v: 0,
+          ...mockIngredientConstructor,
           uniqueId: mockIngredientUniqueId,
         },
       ],
@@ -129,38 +74,8 @@ describe('ingredientsConstructor reducer', () => {
   it('should handle updateIndexIngredients', () => {
     const state: IInitialState = {
       ingredients: [
-        {
-          _id: '643d69a5c3f7b9001cfa093c',
-          name: 'Краторная булка N-200i',
-          type: 'bun',
-          proteins: 80,
-          fat: 24,
-          carbohydrates: 53,
-          calories: 420,
-          price: 1255,
-          image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-          image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-          image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-          __v: 0,
-          uniqueId: '643d69a5c3f7b9001cfa093c',
-          index: 1,
-        },
-        {
-          _id: '643d69a5c3f7b9001cfa093c',
-          name: 'Краторная булка N-200i',
-          type: 'bun',
-          proteins: 80,
-          fat: 24,
-          carbohydrates: 53,
-          calories: 420,
-          price: 1255,
-          image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-          image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-          image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-          __v: 0,
-          uniqueId: '643d69a5c3f7b9001cfa093c',
-          index: 3,
-        },
+        { ...mockIngredientConstructor, index: 1 },
+        { ...mockIngredientConstructor, index: 3 },
       ],
       bunTop: null,
       bunBottom: null,
@@ -179,40 +94,7 @@ describe('ingredientsConstructor reducer', () => {
 
   it('should handle moveIngredients', () => {
     const state: IInitialState = {
-      ingredients: [
-        {
-          _id: '643d69a5c3f7b9001cfa093c',
-          name: 'Краторная булка N-200i',
-          type: 'bun',
-          proteins: 80,
-          fat: 24,
-          carbohydrates: 53,
-          calories: 420,
-          price: 1255,
-          image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-          image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-          image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-          __v: 0,
-          uniqueId: '643d69a5c3f7b9001cfa093c',
-          index: 1,
-        },
-        {
-          _id: '643d69a5c3f7b9001cfa093c',
-          name: 'Краторная булка N-200i',
-          type: 'bun',
-          proteins: 80,
-          fat: 24,
-          carbohydrates: 53,
-          calories: 420,
-          price: 1255,
-          image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-          image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-          image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-          __v: 0,
-          uniqueId: '643d69a5c3f7b9001cfa093c',
-          index: 3,
-        },
-      ],
+      ingredients: [mockIngredientConstructor, mockIngredientConstructor],
       bunTop: null,
       bunBottom: null,
     };
